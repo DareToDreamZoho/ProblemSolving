93afa4dd3450ddc798009059010ba13ca5f5a505
DareToDreamZoho <processzoho@gmail.com>
2024-12-26 19:51:23 +0530

    Add files via upload

---
+++ row_with_maximum1_SK.java
@@ -0,0 +1,21 @@
+public class row_with_maximum1_SK {
+    public static void main(String args[]) {
+        int arr[][]={{0,1,1,1},{0,0,1,1},{1,0,0,1},{0,0,0,0}},maxRow=0,maxOne=-1,l=arr.length,c=0;
+        for(int i=0;i<l;i++){
+          for(int j=0;j<l;j++){
+            if(arr[i][j]==1)
+                c++;
+          }
+         
+          if(c>maxOne){
+            maxOne=c;
+            maxRow=i;
+            
+          }
+          c=0;
+        }
+        System.out.println("\n row:"+maxRow+" (index)");
+
+    }
+}
+        
